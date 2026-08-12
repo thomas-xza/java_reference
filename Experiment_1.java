52e8e51a8f578fe56a2aba8cc83cc41dcdfc817c
Thomas <122806804+thomas-xza@users.noreply.github.com>
2026-08-12 13:41:59 +0000

    Minor update

--- Experiment_1.java
+++ Experiment_1.java
@@ -8,14 +8,40 @@ public class Experiment_1 {
 
 	Inanimate_class c = new Inanimate_class();
 
+	//  Despite being an Integer type, the original object passed in is not modified.
+
  	System.out.println(a);
  	System.out.println(test_pass_by_ref(a));
+	System.out.println(a);
 
  	System.out.println(c.x);
+
+	int i;
+
+	int[] int_array = new int[100];
+	String[] string_array = new String[100];
+
+	for (i = 0; i < 100 ; i++) {
+
+	    int_array[i] = i;
+
+	    // System.out.println(int_array[i]);
+
+	    string_array[i] = "Hello";
+
+	    // System.out.println(string_array[i]);
+
+	}
+
+	//  The whole string is inside on element, unlike a char array.
+
+	// System.out.println(string_array[50]);
 
     }
 
     public static Integer test_pass_by_ref(Integer a) {
+
+	a -= 1;
 
 	Integer b = a;
 
